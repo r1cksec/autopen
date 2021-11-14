f01f59b07b21dff10c5cb614fe1d51741fa114d8
r1cksec <r1cksecurity@protonmail.com>
2021-11-14 20:26:44 +0100

    Initial Commit

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 pathToScriptDir="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-sed -i "s|REPLACEME|${pathToScriptDir}/wordlists|g" "${pathToScriptDir}/modules.json"
+sed -i "s|REPLACEME|${pathToScriptDir}|g" "${pathToScriptDir}/modules.json"
 
 echo "Done"
 
